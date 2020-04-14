4bac2c945a69051ec8a6e6dc4d3ed000fa646d3e
Staffan <id17svd@cs.umu.se>
2020-04-14 14:41:07 +0200

    Survey model implemented

---
+++ models/Survey.js
@@ -0,0 +1,21 @@
+/**
+ * Model class for survey. Represents a survey object that exists in the survey collection.
+ */
+
+const mongoose = require('mongoose');
+
+/**
+ * Defines all fields that a survey has.
+ */
+const surveySchema = mongoose.Schema(
+    {
+        e_id: { type: Number, required: true },
+        end_date: { type: Date, required: true },
+        template_id: { type: mongoose.Schema.ObjectId, required: true },
+        responses: { type: Array, required: true },
+    },
+    // needed because otherwise the collection wont be found.
+    { collection: 'survey' }
+);
+
+module.exports = mongoose.model('survey', surveySchema);
