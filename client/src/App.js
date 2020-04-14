00e4c25fa2e0bebe07523dc0964bff3a4136462d
Viktor Malmedal <id17vml@cs.umu.se>
2020-04-14 11:02:10 +0200

    added auth0 boilerplate and dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import Test from './components/test/test';
+import NavBar from './components/NavBar/NavBar';
+import { useAuth0 } from './react-auth0-spa';
+
+import { Router, Route, Switch } from 'react-router-dom';
+import Profile from './components/Profile/Profile';
+import history from './utils/history';
+import PrivateRoute from './components/PricateRoutes/PrivateRoute';
 
 function App() {
+    const { loading } = useAuth0();
+
+    if (loading) {
+        return <div>loading...</div>;
+    }
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <Test />
-            </header>
+            <Router history={history}>
+                <header className="App-header">
+                    <NavBar />
+                </header>
+                <Switch>
+                    <Route path="/" exact />
+                    <PrivateRoute path="/profile" componet={Profile} />
+                </Switch>
+            </Router>
         </div>
     );
 }
