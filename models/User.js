3d642863433add463e7e7974f8dd82aee87d1599
Staffan <id17svd@cs.umu.se>
2020-04-15 09:57:37 +0200

    user api done

--- models/User.js
+++ models/User.js
@@ -1,5 +1,19 @@
+/**
+ * Model class for user. Represents a user object that exists in the user collection.
+ */
+
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({});
+/**
+ * Defines all fields that a user has.
+ */
+const userSchema = mongoose.Schema({
+    given_name: { type: String, required: true },
+    family_name: { type: String, required: true },
+    role: { type: String, required: true },
+    picture: { type: String, required: false },
+    responses: { type: Array, required: true },
+    email: { type: String, required: true },
+});
 
 module.exports = mongoose.model('users', userSchema);
